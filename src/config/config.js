190809b26c8f3e9538c4f93b2723c67956f7e603
silendas <muhammadyasminnulhakim34@gmail.com>
2024-12-07 15:09:23 +0700

    vercel deploy

--- src/config/config.js
+++ src/config/config.js
@@ -6,11 +6,11 @@ dotenv.config();
 
 const config = {
   db: {
-    database: process.env.NODE_ENV === 'production' ? process.env.DB_DATABASE_PROD : process.env.DB_DATABASE_DEV,
-    username: process.env.NODE_ENV === 'production' ? process.env.DB_USERNAME_PROD : process.env.DB_USERNAME_DEV,
-    password: process.env.NODE_ENV === 'production' ? process.env.DB_PASSWORD_PROD : process.env.DB_PASSWORD_DEV,
-    host: process.env.NODE_ENV === 'production' ? process.env.DB_HOST_PROD : process.env.DB_HOST_DEV,
-    dialect: process.env.NODE_ENV === 'production' ? process.env.DB_DIALECT_PROD : process.env.DB_DIALECT_DEV,
+    database: process.env.POSTGRES_DATABASE,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
   },
   port: process.env.PORT,
 };

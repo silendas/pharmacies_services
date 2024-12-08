c176933a41cc564941ce04f0e5be330649db4a3a
silendas <muhammadyasminnulhakim34@gmail.com>
2024-12-08 22:47:46 +0700

    auth

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const auth = require('../middleware/auth');
 
 // Public routes
 router.post('/login', userController.login);

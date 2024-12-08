a059adb5e1c5786ef3c48262fb4f79267452f86b
silendas <muhammadyasminnulhakim34@gmail.com>
2024-12-08 22:08:53 +0700

    update postgres

--- src/config/config.js
+++ src/config/config.js
@@ -9,7 +9,6 @@ dotenv.config();
 const pool = new Pool({
   user: process.env.POSTGRES_USER, 
   host: process.env.POSTGRES_HOST,
-  database: 'db_pharmacy',
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
 });

6777f423dbc646d47a59cd9cde14e10cf6a391c9
silendas <muhammadyasminnulhakim34@gmail.com>
2024-12-10 10:29:25 +0700

    Otorisasi

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: ['*', 'http://localhost:3000'],
+  origin: ['*', 'http://localhost:3000', 'http://localhost:5173'],
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };

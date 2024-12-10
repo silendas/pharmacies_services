8db48b3769bce8af34a5e4b0268842d6a7642e4c
silendas <muhammadyasminnulhakim34@gmail.com>
2024-12-10 10:32:32 +0700

    Otorisasi

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,11 +9,11 @@ router.post('/login', userController.login);
 // Protected routes
 router.use(auth);
 // Admin only routes
-router.get('/users', userController.getAllUsers);
-router.get('/users/:id', userController.getUserById);
-router.post('/users', userController.createUser);
-router.put('/users/:id', userController.updateUser);
-router.delete('/users/:id', userController.deleteUser);
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUserById);
+router.post('/', userController.createUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 // Regular authenticated routes
 router.post('/logout', userController.logout);

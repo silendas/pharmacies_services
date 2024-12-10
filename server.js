a2c421d66403d660fa66e2b9b0bedc979babdec6
silendas <muhammadyasminnulhakim34@gmail.com>
2024-12-10 08:54:58 +0700

    cors options

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
